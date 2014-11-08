182d4517715697322d924334c5707284e63ff784
Bozhao Yu <yubz86@Bozhaos-MacBook-Air-7.local>
2014-11-07 21:56:52 -0800

    init commit

---
+++ package.js
@@ -0,0 +1,24 @@
+Package.describe({
+  name: 'bozhao:cordova-ble',
+  summary: 'BLE plugin for meteor',
+  version: '0.0.1',
+  git: 'https://github.com/yubozhao/meteor-cordova-ble.git'
+});
+
+Cordova.depends({
+  "com.evothings.ble": "0.0.1"
+});
+
+Package.onUse(function(api) {
+  api.versionsFrom('1.0');
+  api.addFiles('easy-ble.js');
+  api.addFiles('ti-sensortag.js');
+
+  api.export('TISensorTag', 'client');
+  api.export('easyble', 'client');
+});
+
+Package.onTest(function(api) {
+  api.use('tinytest');
+  api.use('bozhao:cordova-ble');
+});
